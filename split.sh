a08b4606253fdbd09a6aedf67555c2c7d5104c89
Kevin Roark <ker2143@columbia.edu>
2016-04-16 16:57:18 -0400

    log of splits

---
+++ split.sh
@@ -0,0 +1,64 @@
+
+### CLIP 17
+#./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip17.m4v media/hardcore4.mp4 5 60
+
+### CLIP 15
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction1.mp4 60 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction2.mp4 120 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction3.mp4 180 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction4.mp4 240 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction5.mp4 300 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction6.mp4 360 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction7.mp4 420 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction8.mp4 480 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction9.mp4 540 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip15.m4v media/construction10.mp4 600 42
+
+### CLIP 12
+#./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip12.m4v media/bird0.mp4 10 57.5
+
+### CLIP 11
+#./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip11.m4v media/bird1.mp4 0 81
+
+### CLIP 10
+#./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip10.m4v media/bus0.mp4 156 45
+
+### CLIP 9
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip9.m4v media/car0.mp4 7 53
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip9.m4v media/car1.mp4 60 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip9.m4v media/car2.mp4 120 59
+
+### CLIP 7
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip7.m4v media/hardcore0.mp4 0 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip7.m4v media/hardcore1.mp4 60 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip7.m4v media/hardcore2.mp4 120 60
+
+### CLIP 6
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip6.m4v media/hardcore3.mp4 0 70
+
+### CLIP 5
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip5.m4v media/car3.mp4 0 15
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip5.m4v media/car4.mp4 20 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip5.m4v media/car5.mp4 80 80
+
+### CLIP 4
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus1.mp4 0 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus2.mp4 60 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus3.mp4 120 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus4.mp4 180 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus5.mp4 240 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus6.mp4 300 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus7.mp4 360 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus8.mp4 420 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus9.mp4 480 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus10.mp4 540 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus11.mp4 600 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus12.mp4 660 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus13.mp4 720 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus14.mp4 780 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus15.mp4 840 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus16.mp4 900 60
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip4.m4v media/bus17.mp4 960 60
+
+### CLIP 0
+# ./cut.sh ~/Pictures/video_camera/april_16_2016_import/Clip0.m4v media/food0.mp4 5 60
